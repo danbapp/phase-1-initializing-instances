51bcc98d4e3ae554d178a230f73a11847866b837
Daniel <danbapp@gmail.com>
2022-01-03 15:23:05 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  #dessert;
+  constructor(soup, salad, entree, dessert) {
+    this.soup = soup;
+    this.salad = salad;
+    this.entree = entree;
+    this.#dessert = dessert;
+  }
+}
+
+const dinner = new Dinner("balsamic salad", "consomme", "filet mignon", "cheesecake");
